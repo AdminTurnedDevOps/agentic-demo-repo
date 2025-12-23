32fabb36f26e7a6207f1b0f7c2a455889222124b
adminturneddevops <mlevan1992@gmail.com>
2025-12-23 13:29:23 -0500

    EKS TF

---
+++ eks-terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "The AWS region to create resources in."
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  default     = "bedrocktest"
+}
+
+variable "cluster_version" {
+  description = "The Kubernetes version for the EKS cluster."
+  default     = "1.33"
+}
+
+variable "pub_subnet_id_1" {
+  type = string
+  default = "subnet-0d2ea8898cdc199af"
+}
+
+variable "pub_subnet_id_2" {
+  type = string
+  default = "subnet-08f1c1b07f89afd3e"
+}
